be4f45886e9ee8283ccb61e22a475a395db4688b
Melissa Stock <melissastock44@gmail.com>
2019-10-10 15:47:47 -0700

    Creatied game and pacperson class

---
+++ pacman/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace pacman
+{
+    public class Game
+    {
+        ConsoleKeyInfo userInput;
+        public int cookies = 10;
+
+        //public Game()
+        //{
+        //    Board = new Board();
+        //}
+
+        public static void PlayGame()
+        {
+            Game currentGame = new Game();
+            PacPerson currentPacPerson = new PacPerson();
+            while (currentGame.cookies > 0)
+            {
+                currentGame.GetUserInput(currentPacPerson);
+                currentGame.MovePacPerson(currentPacPerson);
+                currentGame.cookies--;
+                Console.WriteLine(currentGame.cookies);
+            }
+        }
+
+        private void GetUserInput(PacPerson pacperson)
+        {
+            do
+            {
+                Console.WriteLine("Use the arrow keys to choose the direction you would like to move PacMan");
+                userInput = Console.ReadKey(true);
+                pacperson.ChangeDirection((int)userInput.Key);
+            }
+                while ((int)userInput.Key < 37 || (int)userInput.Key > 40);
+        }
+
+        private void MovePacPerson(PacPerson pacperson)
+        {
+            if (pacperson.currentDirection == Direction.RightArrow)
+            {
+                //currentGame.UpdateBoard(0 column++);
+            }
+            else if (pacperson.currentDirection == Direction.LeftArrow)
+            {
+                //currentGame.UpdateBoard(0, column--);
+            }
+            else if (pacperson.currentDirection == Direction.UpArrow)
+            {
+                //currentGame.UpdateBoard(row--, 0);
+            }
+            else
+            {
+                //currentGame.UpdateBoard(row++, 0);
+            }
+        }
+    }
+}
---
+++ pacman/PacPerson.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace pacman
+{
+    public enum Direction { LeftArrow = 37, UpArrow = 38, RightArrow = 39, DownArrow = 40};
+
+public class PacPerson
+    {
+        public Direction currentDirection { get; set; }
+
+        public PacPerson()
+        {
+            currentDirection = Direction.RightArrow;
+        }
+
+        public void ChangeDirection(int userInput)
+        {
+            switch (userInput)
+            {
+                case (int)Direction.LeftArrow:
+                    currentDirection = Direction.LeftArrow;
+                    break;
+                case (int)Direction.UpArrow:
+                    currentDirection = Direction.UpArrow;
+                    break;
+                case (int)Direction.DownArrow:
+                    currentDirection = Direction.DownArrow;
+                    break;
+                case (int)Direction.RightArrow:
+                    currentDirection = Direction.RightArrow;
+                    break;
+                default: 
+                    break;
+            }
+        }
+    }
+}
